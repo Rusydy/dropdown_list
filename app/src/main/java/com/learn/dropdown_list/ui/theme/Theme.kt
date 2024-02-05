13bc8d3fe3e2201a989b3ba1ac0b12a28a2d5b23
Rusydy <rusydymuhiddin@gmail.com>
2024-02-05 23:52:52 +0700

    Rename Dropdown_listTheme to DropDownListTheme

--- app/src/main/java/com/learn/dropdown_list/ui/theme/Theme.kt
+++ app/src/main/java/com/learn/dropdown_list/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Dropdown_listTheme(
+fun DropDownListTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
