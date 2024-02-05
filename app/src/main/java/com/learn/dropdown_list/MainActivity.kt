a4ba4ee1431b5ba777b929325e965312931902c0
Rusydy <rusydymuhiddin@gmail.com>
2024-02-05 23:52:59 +0700

    Add dropdown list functionality

--- app/src/main/java/com/learn/dropdown_list/MainActivity.kt
+++ app/src/main/java/com/learn/dropdown_list/MainActivity.kt
@@ -3,26 +3,34 @@ package com.learn.dropdown_list
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.learn.dropdown_list.ui.theme.Dropdown_listTheme
+import androidx.compose.ui.unit.dp
+import com.learn.dropdown_list.ui.theme.DropDownListTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Dropdown_listTheme {
+            DropDownListTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DropDownList()
                 }
             }
         }
@@ -30,17 +38,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun DropDownList(
+    modifier: Modifier = Modifier,
+) {
+    // list of strings
+    val items = listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        for (item in items) {
+            Greeting(name = item)
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+
+        Row(
+            modifier = modifier.padding(24.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+
+            ) {
+                Text(text = "Hello")
+                Text(text = name)
+            }
+
+            ElevatedButton(
+                onClick = { expanded.value = !expanded.value },
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Dropdown_listTheme {
-        Greeting("Android")
+fun DropDownListPreview() {
+    DropDownListTheme {
+        DropDownList()
     }
 }
